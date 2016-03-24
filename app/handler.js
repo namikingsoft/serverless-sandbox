2c950d6392e9ea648856a640df4673f577360db1
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-24 12:29:02 +0900

    chore(build): change directory structure

--- app/handler.js
+++ app/handler.js
@@ -1,5 +1,5 @@
 require('source-map-support').install()
-const lib = require('../../lib')
+const lib = require('./lib')
 
 module.exports.handler = (event, context) => {
   lib(event, (err, data) => context.done(err, data))
